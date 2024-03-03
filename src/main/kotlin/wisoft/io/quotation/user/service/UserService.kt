38c6f6c2ea812f6f047ed518628aa4a43462bf21
Kukjun Lee <lxx3380@gmail.com>
2024-03-03 13:03:46 +0900

    feature : jwt, password crypt
    jwt 발급, password 암호화를 위해 의존을 당겨오고 해당 기능을 이용해서 로그인을 구현함

--- src/main/kotlin/wisoft/io/quotation/user/service/UserService.kt
+++ src/main/kotlin/wisoft/io/quotation/user/service/UserService.kt
@@ -1,10 +1,13 @@
 package wisoft.io.quotation.user.service
 
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import wisoft.io.quotation.user.api.request.UserSigninRequest
 import wisoft.io.quotation.user.service.dto.UserTokenDto
-import java.lang.RuntimeException
+import wisoft.io.quotation.user.util.JWTUtil
+import kotlin.RuntimeException
+
 
 @Service
 class UserService(
@@ -12,12 +15,15 @@ class UserService(
 ) {
     fun signin(request: UserSigninRequest): UserTokenDto {
         val user = userRepository.findByIdOrNull(request.id) ?: throw RuntimeException()
-        // TODO : Password 알고리즘 적용 후 비교
 
-        // TODO : 비교 완료 후, user 정보 기반으로 토큰 생성 후 반환
+        val encoder = BCryptPasswordEncoder()
+        val isMatch = encoder.matches(request.password, user.password)
+        if(!isMatch) throw RuntimeException()
 
-        // FIXME : 예비 반환 값을 실제 반환 값으로 변환
-        return UserTokenDto("test", "test")
+        val accessToken = JWTUtil.generateAccessToken(user)
+        val refreshToken = JWTUtil.generateRefreshToken(user)
+
+        return UserTokenDto(accessToken, refreshToken)
 
     }
 }
